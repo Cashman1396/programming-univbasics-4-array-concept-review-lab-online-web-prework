e1f40d353fc1268d0a0e3edeb9d8ea6787e65f5c
Chris Ashman <chrisashman96@gmail.com>
2020-01-11 23:35:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,18 +3,14 @@ def find_element_index(array, value_to_find)
     if array|count| == value_to_find
       return count
 end
-end
-nil
-end
+
 def find_max_value(array)
   max = 0
   array.length.times do |count|
     if array|count| > max
       max = array[count]
 end
-end 
-max
-end
+
 
 def find_min_value(array)
   min = 0
