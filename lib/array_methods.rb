7f3992869056e6c87937c73fc18036f4b9e2c23c
Chris Ashman <chrisashman96@gmail.com>
2020-01-11 23:24:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,7 @@ def find_max_value(array)
   max = 0
   array.length.times do |count|
     if array|count| > max
-      max = array|count|
+      max = array[count]
 end
 
 def find_min_value(array)
